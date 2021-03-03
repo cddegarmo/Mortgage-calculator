c98bf54e09e04f5ea04801501bc689f1d03db6c8
Dillon DeGarmo <cddegarmo@gmail.com>
2021-03-03 16:12:22 -0600

    Add class version of calculator

--- src/Mortgage.java
+++ src/Mortgage.java
@@ -39,6 +39,47 @@ public class Mortgage {
             throw new IllegalStateException();
     }
 
+    public static int getMaxMortgages()        { return MAX_MORTGAGES; }
+    public static int getLoans()               { return loans;         }
+    public static Set<Mortgage> getInventory() { return inventory;     }
+
+    public int getSalePrice()                  { return salePrice;     }
+    public int getDownPayment()                { return down;          }
+    public double getInterest()                { return interest;      }
+    public int getPeriod()                     { return period;        }
+    public int getPrincipal()                  { return principal;     }
+    public double getPropertyTax()             { return propertyTax;   }
+
+    private double monthlyInterest() {
+        double result = interest / Utility.AS_PERCENT / Utility.MONTHS;
+        return result;
+    }
+
+    private int getMonths() {
+        if(period == 20)
+            return Utility.MONTHS_20;
+        else if(period == 30)
+            return Utility.MONTHS_30;
+        else
+            return period * Utility.MONTHS;
+    }
+
+    private double getTaxes() {
+        return propertyTax / Utility.AS_PERCENT *
+                principal / Utility.MONTHS;
+    }
+
+    private double getMonthlyPayment() {
+        double result = principal * ((monthlyInterest() * Math.pow(monthlyInterest() + 1, getMonths())) /
+                (Math.pow(monthlyInterest() + 1, getMonths()) - 1)) + getTaxes();
+        return result;
+    }
+
+    public void getAsCurrency() {
+        double amount = getMonthlyPayment();
+        System.out.format("%.2f", amount);
+    }
+
     public static void addInventory(Mortgage mg) {
         inventory.add(mg);
     }
@@ -53,5 +94,18 @@ public class Mortgage {
                 mg.interest == interest && mg.propertyTax == propertyTax;
     }
 
-    
+    public int hashCode() {
+        int result = Integer.hashCode(salePrice);
+        result = 31 * result + Integer.hashCode(down);
+        result = 31 * result + Double.hashCode(interest);
+        result = 31 * result + Double.hashCode(propertyTax);
+        return result;
+    }
+
+    public String toString() {
+        return String.format("Sale price is $%,d and loan amount is $%,d." +
+                "\nWith an interest rate of %.2f and property taxes of %.2f percent, " +
+                "the monthly payments are $%,.2f.", salePrice, principal, interest,
+                propertyTax, getMonthlyPayment());
+    }
 }
--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -1,3 +1,5 @@
+// Contents of this file sourced from YouTube tutorial on Java basics.
+
 import java.util.Scanner;
 import java.text.NumberFormat;
 
@@ -8,30 +10,24 @@ public class MortgageCalculator {
 
         Scanner sc = new Scanner( System.in );
 
-        // Set principal of loan
-        System.out.print( "Principal: " );
+        System.out.print("Principal: ");
         int principal = sc.nextInt();
 
-        // Convert annual interest rate into monthly interest rate
-        System.out.print( "Annual interest rate: " );
+        System.out.print("Annual interest rate: ");
         double interest = sc.nextDouble();
         double monthlyInterest = interest / percentDivider / monthsInYear;
 
-        // Convert payment period into months
-        System.out.print( "Payment period (years): " );
+        System.out.print("Payment period (years): ");
         int years = sc.nextInt();
         int months = years * monthsInYear;
 
-        // Calculate property taxes (applicable to states with such taxes)
-        System.out.print( "Property taxes (percentage): ");
+        System.out.print("Property taxes (percentage): ");
         double taxes = sc.nextDouble();
         double monthlyTaxes = taxes / percentDivider * principal / monthsInYear;
 
-        // Calculate mortgage plus monthly property taxes
-        double mortgage = principal * ( ( monthlyInterest * Math.pow( monthlyInterest + 1, months ) ) /
-                ( Math.pow( monthlyInterest + 1, months ) - 1 ) ) + monthlyTaxes;
+        double mortgage = principal * ((monthlyInterest * Math.pow(monthlyInterest + 1, months)) /
+                (Math.pow(monthlyInterest + 1, months) - 1)) + monthlyTaxes;
 
-        // Format value to U.S. dollar (USD) and print to console
         String mortgageInDollars = NumberFormat.getCurrencyInstance().format( mortgage );
         System.out.println( "Monthly payment: " + mortgageInDollars );
     }
